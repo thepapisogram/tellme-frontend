42207a1fe1eb3adfae915186ee522f5328470431
Anthony Saah <thepapisogram@gmail.com>
2024-12-11 10:43:13 +0000

    logo component updated

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Logo({ size = 200 }:{size?:number}) {
   return (
-    <div className={`flex justify-center w-full h-full`}>
+    <div className={`flex justify-center w-[200px] h-[100px] mx-auto`}>
       <Image
         src="/dark.webp"
         className="rounded-badge w-auto h-auto"
